22dfa27162d6611992f2e125c3c8dbd488457a8f
Jefferson Rodrigues do Nascimento <111389059+jge173@users.noreply.github.com>
2025-08-05 22:25:30 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import requests
 from PIL import Image
 import io
+import inference_sdk from inferenceHTTPClient
 
 st.set_page_config(page_title="Agente de IA Financeiro", layout="wide")
 st.title("📊 Análise de Padrões de Velas com IA")
@@ -48,6 +49,7 @@ if uploaded_file:
 
 st.markdown("---")
 st.caption("Versão 1.0 • Desenvolvido por Jefferson • Modelo hospedado via Roboflow")
+
 
 
 
